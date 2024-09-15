7c901fd2f3e847225f0aa63d0a47eb54a5eb1e6d
Your Name <you@example.com>
2024-09-15 15:37:41 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+//CONVERT STRING  TO A NUMBER   
+let price = "200";
+let priceparse = parseInt(price);
+console.log('Price: ',priceparse);
+
+let number = Number(price);
+console.log(number);
+
+let plussSign = + price;
+console.log(plussSign);
+
+//CONVERT NUMBER TO A STRING
+let Distance = 150;
+let theDistance = Distance.toString();
+console.log(theDistance);
+let whatDistance = String(Distance);
+console.log(whatDistance);
+
+//CONVERT STRING TO DECIMAL NUMBER
+let Weight = "72.5";
+let WeightParse = parseFloat(Weight);
+console.log(WeightParse);
